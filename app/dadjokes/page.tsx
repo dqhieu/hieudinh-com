8af96bd972f10e9ded69b53f65c93ad70e6e6239
dqhieu <dqhieu13@gmail.com>
2023-07-27 21:58:15 +0700

    update site

---
+++ app/dadjokes/page.tsx
@@ -0,0 +1,30 @@
+export default function Page() {
+  return (
+    <div>
+    <h1>Privacy Policy for Dad Jokes & Puns App</h1> 
+    <p>At Dad Jokes &amp; Puns, we value your privacy and are committed to protecting your personal information.  This Privacy Policy explains how we collect, use, and safeguard your data when you use our app.
+    </p> 
+    <h2>1. Information We Collect:</h2> 
+    <ul> 
+      <li>Automatically Collected Information: We may collect certain information automatically when you use the app, including your device's identifier, operating system, and usage data. This helps us analyze trends, monitor the performance of our app, and improve our services.</li> 
+      </ul> 
+    <h2>2. Use of Information:</h2> 
+    <ul> 
+      <li>Personalization: We may use the information you provide to personalize your experience on Dad Jokes &amp; Puns, such as recommending jokes based on your preferences and providing customized content.</li> 
+      <li>Statistical Analysis: We use collected information to analyze usage patterns, aggregate user demographics, and enhance the user experience.</li> 
+    </ul> 
+    <h2>3. Sharing of Information:</h2> 
+    <ul> 
+      <li>Third-Party Service Providers: We may share certain information with trusted third-party service providers who assist us in operating the app, managing user data, and delivering services to you. These providers are required to maintain the confidentiality and security of your information.</li> 
+      <li>Legal Compliance: Dad Jokes &amp; Puns may disclose your information if required by law or if we believe, in good faith, that such action is necessary to comply with legal obligations, protect our rights, or respond to legal requests.</li> 
+    </ul> 
+    <h2>4. Data Security:</h2> 
+    <p>We strive to protect your personal information and have implemented technical and organizational measures to safeguard it. However, no transmission over the internet or electronic storage is completely secure, so we cannot guarantee absolute security of your data.</p> 
+    <h2>6. Changes to this Privacy Policy:</h2> 
+    <p>Dad Jokes &amp; Puns reserves the right to modify or update this Privacy Policy at any time. We will notify you of any material changes through a prominent notice on our app or by other means before the changes take effect.</p>
+     <p>By using Dad Jokes &amp; Puns, you consent to the collection, use, and disclosure of your information as described in this Privacy Policy.</p> 
+     <p>If you have any questions or concerns about this Privacy Policy, please contact us at <a href="mailto:dadjokes.support@hieudinh.com">dadjokes.support@hieudinh.com</a>.</p> 
+     <p>Last updated: July 27, 2023</p>
+    </div>
+  )
+}
