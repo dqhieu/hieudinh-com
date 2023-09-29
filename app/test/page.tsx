3247fab71cc05c6d56b768eaebcc1c1a8acbac61
dqhieu <dqhieu13@gmail.com>
2023-09-29 22:57:08 +0800

    Test redirect

---
+++ app/test/page.tsx
@@ -0,0 +1,11 @@
+import { Metadata } from 'next'
+import { redirect } from 'next/navigation'
+
+ 
+export const metadata: Metadata = {
+  title: 'Dad Jokes & Puns',
+}
+
+export default function Page() {
+  redirect('/dadjokes')
+}
