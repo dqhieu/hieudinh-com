62f33ee09965097db8e2d89e35794cbb172ddafe
dqhieu <dqhieu13@gmail.com>
2023-12-04 00:15:02 +0800

    Handle case insensitive

---
+++ src/middleware.tsx
@@ -0,0 +1,13 @@
+import { NextResponse, NextRequest } from "next/server"
+
+const Middleware = (req: NextRequest) => {
+    if (req.nextUrl.pathname === req.nextUrl.pathname.toLowerCase()) {
+        return NextResponse.next()
+    }
+
+    return NextResponse.redirect(
+        new URL(req.nextUrl.origin + req.nextUrl.pathname.toLowerCase())
+    )
+}
+
+export default Middleware
