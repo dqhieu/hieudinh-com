926d65c0d6a379f3daddd5195aca30051619c373
dqhieu <dqhieu13@gmail.com>
2023-12-08 10:12:53 +0800

    Fix build

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ export default function Home() {
     <main>
       
       <div className="min-h-screen flex flex-col items-center justify-center font:sans-serif bg-zinc-100">
-        <p>Hi, I'm Hieu Dinh</p>
+        <p>Hi, I’m Hieu Dinh</p>
         <p>I makes app for  ecosystem</p>
         <p>Check out my works on <a href='https://x.com/hieudinh_'>𝕏</a></p>
       </div>
