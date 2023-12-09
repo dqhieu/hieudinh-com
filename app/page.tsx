ce86e3e3b787d633029e941ae53ba5e399430c6b
dqhieu <dqhieu13@gmail.com>
2023-12-09 10:00:46 +0800

    Update landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,25 @@
+import Image from 'next/image'
+import swiftGif from 'src/images/wwdc-swift.gif'
+
 export default function Home() {
   return (
     <main>
       
-      <div className="min-h-screen flex flex-col items-center justify-center font:sans-serif bg-zinc-100">
-        <p>Hi, I’m Hieu Dinh</p>
-        <p>I makes app for  ecosystem</p>
-        <p>Check out my works on <a href='https://x.com/hieudinh_'>𝕏</a></p>
+      <div className="min-h-screen flex flex-col items-center justify-center font:sans-serif bg-black">
+        <Image
+          className="fixed"
+          src={swiftGif}
+          alt="background image"
+          priority
+          width={1558}
+          height={946}
+          unoptimized
+        />
+        <div className="absolute flex flex-col items-center justify-center text-2xl text-white top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+          <p>Hi, I’m Hieu Dinh</p>
+          <p>I makes app for  ecosystem</p>
+         <p>Check out my works on <a href='https://x.com/hieudinh_'>𝕏</a></p>
+        </div>
       </div>
 
     </main>
