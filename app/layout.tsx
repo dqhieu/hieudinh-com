dec9ddf587ff051ce146d353fb95bc66ce6731f1
dqhieu <dqhieu13@gmail.com>
2023-12-09 13:14:45 +0800

    Add analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import Script from 'next/script'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -15,6 +16,12 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <Script 
+          src="https://cdn.telemetrydeck.com/websdk/telemetrydeck.min.js"
+          data-app-id="1565CA38-B4D3-44C5-ABE8-F980E0EB9A2C"
+        />
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   )
