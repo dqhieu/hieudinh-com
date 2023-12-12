749ec398a91254301750e5cace71e3a15b28a22c
dqhieu <dqhieu13@gmail.com>
2023-12-13 02:07:57 +0800

    Fix typo

--- app/page.tsx
+++ app/page.tsx
@@ -17,7 +17,7 @@ export default function Home() {
         />
         <div className="absolute flex flex-col items-center justify-center text-2xl text-white top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
           <p>Hi, I’m Hieu Dinh</p>
-          <p>I makes app for  ecosystem</p>
+          <p>I make apps for  ecosystem</p>
          <p>Check out my works on <a href='https://x.com/hieudinh_'>𝕏</a></p>
         </div>
       </div>
