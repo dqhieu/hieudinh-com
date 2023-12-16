7958e337cb6e055d0d608e791d855962d3a4f740
dqhieu <dqhieu13@gmail.com>
2023-12-16 21:35:56 +0800

    test react-tweet

--- app/compressx/page.tsx
+++ app/compressx/page.tsx
@@ -1,10 +1,19 @@
-import { redirect } from 'next/navigation'
 import { Metadata } from 'next'
- 
+import { Tweet } from 'react-tweet'
+
 export const metadata: Metadata = {
   title: 'CompressX',
 }
 
+// export default function Page() {
+//   redirect('https://hieudinh.lemonsqueezy.com/checkout/buy/572b7285-ab58-48b5-a919-036c7787c30b')
+// }
+
+
 export default function Page() {
-  redirect('https://hieudinh.lemonsqueezy.com/checkout/buy/572b7285-ab58-48b5-a919-036c7787c30b')
+  return (
+    <div>
+      <Tweet id="1705155679819084179" />
+    </div> 
+  )
 }
