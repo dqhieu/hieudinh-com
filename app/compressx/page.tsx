a25d646eee7b1abf4683b9495945613dc6cb9ff6
dqhieu <dqhieu13@gmail.com>
2023-12-23 13:11:58 +0800

    Update landing page with video

--- app/compressx/page.tsx
+++ app/compressx/page.tsx
@@ -24,9 +24,8 @@ const LemonSqueezyLogo = () => {
 
 const VideoPlayer = () => {
   return (
-    <video controls autoPlay muted loop playsInline>
+    <video className="rounded-xl shadow-2xl" autoPlay muted loop playsInline>
       <source src="/compressx/demo.mp4" type="video/mp4" />
-      Your browser does not support the video tag.
     </video>
   );
 };
@@ -141,6 +140,18 @@ export default function Page() {
               }}
             />
           </div>
+          <div
+            className="absolute inset-x-0 bottom-0 -z-10 flex transform-gpu overflow-hidden pt-32 opacity-25 blur-3xl sm:pt-40 xl:justify-end"
+            aria-hidden="true"
+          >
+            <div
+              className="ml-[-22rem] aspect-[1313/771] w-[62.0625rem] flex-none origin-top-right rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] xl:ml-0 xl:mr-[calc(50%-12rem)]"
+              style={{
+                clipPath:
+                  'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
+              }}
+            />
+          </div>
           <div className="mx-auto max-w-7xl px-6 lg:px-8">
             <div className="mx-auto max-w-xl text-center">
               <h1 className="text-4xl font-bold leading-8 tracking-tight dark:text-white">CompressX</h1>
@@ -156,12 +167,12 @@ export default function Page() {
                 </div>
               </a>
               <p className='text-sm text-gray-500 mt-2'>macOS 13+ is requried</p>
-              <div className='mt-4 rounded-full'>
+              <div className='mt-4'>
                 <VideoPlayer/>
               </div>
             </div>
             <div className="mx-auto max-w-xl xl:max-w-6xl">
-              <dl className="mt-12 space-y-2 sm:grid sm:grid-cols-2 md:grid-cols-2 sm:gap-x-6 sm:gap-y-12 sm:space-y-0 xl:grid-cols-4">
+              <dl className="mt-12 space-y-2 sm:grid sm:grid-cols-2 md:grid-cols-2 sm:gap-x-6 sm:gap-y-6 sm:space-y-0 xl:grid-cols-4">
                 {features.map((feature) => (
                   <div key={feature} className="relative flex items-center">
                     <CheckCircleIcon className=" h-6 w-6 text-green-500" aria-hidden="true" />
@@ -172,7 +183,7 @@ export default function Page() {
             </div>
             <div className="mx-auto max-w-xl text-center">
               <p className="mt-12 text-lg tracking-tight text-gray-900 dark:text-slate-200">
-                Trusted by 1900+ users
+                Trusted by 1900+ users 🌟
             </p>
             </div>
             <div className="mx-auto sm:mt-8 grid max-w-2xl grid-cols-1 grid-rows-1 gap-8 text-sm leading-6 text-gray-900 sm:grid-cols-2 xl:mx-0 xl:max-w-none xl:grid-flow-col xl:grid-cols-4">
