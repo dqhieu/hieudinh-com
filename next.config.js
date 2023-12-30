59b8a24a55b3297e35b99ff61bd1b9f023184505
dqhieu <dqhieu13@gmail.com>
2023-12-30 21:33:51 +0800

    Revert "Test cache"
    This reverts commit 32aad8cd7b071a60b2e7b8e3e26196219a851807.
    
    # Conflicts:
    #	app/compressx/page.tsx

--- next.config.js
+++ next.config.js
@@ -1,10 +1,4 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
-  reactStrictMode: false,
-  images: {
-    remotePatterns: [
-      { protocol: 'https', hostname: 'pbs.twimg.com' },
-      { protocol: 'https', hostname: 'abs.twimg.com' },
-    ],
-  },
+  reactStrictMode: false
 }
