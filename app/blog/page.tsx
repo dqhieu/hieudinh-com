d1993cb7eeb14cccde2904f6dc1eee41dbf70658
dqhieu <dqhieu13@gmail.com>
2025-01-27 13:14:34 +0700

    Update layout

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -10,13 +10,13 @@ export default function BlogPage() {
     <div className="max-w-4xl mx-auto py-12 px-4">
       <Image src={swiftBanner} alt="Swift Banner" className="w-16 h-auto mb-8 rounded-xl" />
       <h1 className="text-3xl font-bold mb-4">Swift things</h1>
-      <div className="space-y-2">
+      <div className="grid grid-cols-1 md:grid-cols-2">
         {posts.map((post) => (
           <article key={post.slug}>
-            <Link href={`/blog/${post.slug}`} className="block">
-              <div className="p-4 -mx-4 rounded-xl hover:bg-neutral-100 dark:hover:bg-gray-800 transition-colors cursor-pointer">
+            <Link href={`/blog/${post.slug}`} className="block  h-full p-4">
+              <div className="p-4 h-full -mx-4 rounded-xl hover:bg-neutral-100 dark:hover:bg-gray-800 transition-colors cursor-pointer">
                 <h2 className="text-xl font-semibold mb-2">{post.title}</h2>
-                <div className="flex items-center gap-4">
+                <div className="flex flex-col gap-2">
                   <time className="text-gray-500 dark:text-gray-400">
                     {new Date(post.date).toLocaleDateString('en-US', {
                       year: 'numeric',
@@ -24,7 +24,7 @@ export default function BlogPage() {
                       day: 'numeric'
                     })}
                   </time>
-                  <div className="flex gap-2">
+                  <div className="flex flex-wrap gap-2">
                     {post.tags.map(tag => (
                       <span key={tag} className="bg-gray-100 dark:bg-gray-800 text-orange-600 dark:text-orange-400 text-sm px-2 py-1 rounded">
                         {tag}
